3c16f0ec6b2e6707275196cbeeca39bb2654615d
Stephan Constantial <Sconstantial@yahoo.com>
2018-07-13 13:43:26 +0100

    added class

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,4 +1,63 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Inheritance(strategy = InheritanceType.JOINED)
+
 public abstract class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public Employee(){
+
+    }
+    public Employee( String firstName, String lastName, double salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id" )
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name")
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstname) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastname) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "salary") public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
+
+
---
+++ src/main/java/models/Programme.java
@@ -0,0 +1,95 @@
+package models;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "programmes")
+@Inheritance(strategy = InheritanceType.JOINED)
+
+public class Programme {
+    private int id;
+    private String title;
+    private int duration;
+    private Producer producer;
+    private Ads ads;
+    private List<Presenter> presenters;
+
+    public Programme(){
+
+    }
+
+
+    public Programme (String title, int duration, Producer producer, Ads ads) {
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+        this.producer = producer;
+        this.ads = ads;
+        this.presenters = new ArrayList<Presenter>();
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title") public String getName() {
+        return title;
+    }
+
+    public void setName(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "duration") public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Column(name = "producer") public Producer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+
+    @Enumerated(value = EnumType.STRING)
+    public Ads getAds() {
+        return ads;
+    }
+
+    public void setAds(Ads ads) {
+        this.ads = ads;
+    }
+
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @ManyToMany
+    @JoinTable(name = "programme_presenter",
+            joinColumns = {@JoinColumn(name = "programme_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "presenter_id", nullable = false, updatable = false)})
+    public List<Presenter> getPresenters() {
+        return presenters;
+    }
+
+    public void setPresenters(List<Presenter> presenters) {
+        this.presenters = presenters;
+    }
+    public void addPresenter(Presenter presenter){
+        this.presenters.add(presenter);
+    }
+}
